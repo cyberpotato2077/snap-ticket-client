a2f40581fe1cf7319d8c36cac2ffe882455649d7
cyberpotato2077 <cyberpotato96@gmail.com>
2025-06-14 03:12:37 +0900

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,30 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { Routes } from "@/constants/Routes";
-import { useRouter } from "next/navigation";
+import { Card } from "@radix-ui/themes";
+import { EventCard } from "./event-card";
+import { EventCarousel } from "./event-carousel";
 
 export default function Home() {
-	const router = useRouter();
-
 	return (
-		<main className="min-h-screen bg-white">
-			{/* 공연 리스트 */}
-			<section className="p-6">
-				<h2 className="text-2xl font-semibold mb-4">공연 목록</h2>
-				<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-					{[...Array(4)].map((_, i) => (
-						<Card key={i} className="w-full">
-							<div className="aspect-square bg-gray-200 flex items-center justify-center text-red-500 text-lg font-bold">
-								공연 이미지
-							</div>
-							<CardContent className="mt-2">
-								<div className="text-red-500 font-semibold">공연 제목</div>
-							</CardContent>
-							<CardFooter>
-								<Button
-									className="w-full"
-									variant="outline"
-									onClick={() =>
-										router.push(Routes.이벤트_상세({ id: String(i) }))
-									}
-								>
-									예매하기
-								</Button>
-							</CardFooter>
-						</Card>
-					))}
+		<main className="min-h-screen bg-white p-6">
+			<div className="grid grid-cols-12 gap-4">
+				<div className="col-span-9">
+					<EventCarousel />
+					{/* 공연 리스트 */}
+					<section className="my-5">
+						<h2 className="text-2xl font-semibold mb-4">공연 목록</h2>
+						<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+							{[...Array(10)].map((_, i) => (
+								<EventCard key={i} />
+							))}
+						</div>
+					</section>
 				</div>
-			</section>
+				<div className="col-span-3">
+					<div className="top-5 sticky">
+						<Card className="w-full">hi</Card>
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 }
